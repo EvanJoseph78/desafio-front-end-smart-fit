70c6b587819bb54490c54f1ad7f3269fb62adde7
Evan Joseph <evandromariano49@gmail.com>
2024-01-29 05:08:06 -0300

    feat: created unit-response.interface

--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GetUnitsService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  getAllUnits(): Observable<UnitsResponse>;
 }
