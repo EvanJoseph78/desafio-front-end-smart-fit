be17a7fd9d03402c4ba0cedddb7dc925e2e11672
Evan Joseph <evandromariano49@gmail.com>
2024-01-29 04:10:26 -0300

    style: creat form structure

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
-
+  resuts = [];
 }
