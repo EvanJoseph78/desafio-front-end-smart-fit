4ef6c152cb626ef684d04d00fe55237614a4cb46
Evan Joseph <evandromariano49@gmail.com>
2024-01-30 01:12:16 -0300

    feat: lógica cards-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { BehaviorSubject } from 'rxjs';
+import { GetUnitsService } from './services/get-units.service';
+import { Location } from './types/location.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'desafio-smartfit';
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService) { }
+
+  onSubmit() {
+    this.showList.next(true);
+    this.unitsList = this.unitService.getFilteredUnits();
+  }
 }
